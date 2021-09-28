f8eecdafb1c72866d622edc7e170aef9d26cdae6
谢琨 <a418165301@163.com>
2021-09-28 15:18:04 +0800

    init repo

---
+++ src/main/java/com/xiekun/cat/SmellyCatApplication.java
@@ -0,0 +1,13 @@
+package com.xiekun.cat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmellyCatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmellyCatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiekun/cat/SmellyCatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiekun.cat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmellyCatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
