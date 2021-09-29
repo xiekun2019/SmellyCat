b08b36ba4eb498cd431781e9d86518a1842e3cff
KunXie <a418165301@163.com>
2021-09-29 18:33:45 +0800

    用户登录，利用 cookie 和 session 记住用户的登录状态

--- src/main/java/com/xiekun/cat/controller/AuthorizeController.java
+++ src/main/java/com/xiekun/cat/controller/AuthorizeController.java
@@ -1,19 +1,18 @@
 package com.xiekun.cat.controller;
 
 import com.xiekun.cat.dto.GiteeAccessTokenDTO;
-import com.xiekun.cat.dto.GithubAccessTokenDTO;
 import com.xiekun.cat.dto.GithubUser;
 import com.xiekun.cat.mapper.UserMapper;
 import com.xiekun.cat.model.User;
 import com.xiekun.cat.provider.GiteeProvider;
-import com.xiekun.cat.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -39,7 +38,7 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           HttpServletRequest request) {
+                           HttpServletResponse response) {
         GiteeAccessTokenDTO accessTokenDTO = new GiteeAccessTokenDTO();
         accessTokenDTO.setGrant_type(grantType);
         accessTokenDTO.setCode(code);
@@ -48,16 +47,18 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         String accessToken = giteeProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = giteeProvider.getUser(accessToken);
+        // 验证成功，获取到了 GithubUser 对象
         if (githubUser != null) {
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
             // 登录成功，写 cookie 和 session
-            request.getSession().setAttribute("user", githubUser);
+            response.addCookie(new Cookie("token", token));
             return "redirect:/";
         } else {
             // 登录失败，重新登录
