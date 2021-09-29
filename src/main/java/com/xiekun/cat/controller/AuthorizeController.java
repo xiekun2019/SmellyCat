59fc8516f4d300be3975e576a40ab654c4b44e83
KunXie <a418165301@163.com>
2021-09-29 18:01:14 +0800

    从 gitee 上获取用户信息存储到数据库中

--- src/main/java/com/xiekun/cat/controller/AuthorizeController.java
+++ src/main/java/com/xiekun/cat/controller/AuthorizeController.java
@@ -3,6 +3,8 @@ package com.xiekun.cat.controller;
 import com.xiekun.cat.dto.GiteeAccessTokenDTO;
 import com.xiekun.cat.dto.GithubAccessTokenDTO;
 import com.xiekun.cat.dto.GithubUser;
+import com.xiekun.cat.mapper.UserMapper;
+import com.xiekun.cat.model.User;
 import com.xiekun.cat.provider.GiteeProvider;
 import com.xiekun.cat.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -31,6 +34,9 @@ public class AuthorizeController {
     @Value("${gitee.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            HttpServletRequest request) {
@@ -41,10 +47,17 @@ public class AuthorizeController {
         accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setRedirect_uri(redirectUri);
         String accessToken = giteeProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = giteeProvider.getUser(accessToken);
-        if (user != null) {
+        GithubUser githubUser = giteeProvider.getUser(accessToken);
+        if (githubUser != null) {
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
             // 登录成功，写 cookie 和 session
-            request.getSession().setAttribute("user", user);
+            request.getSession().setAttribute("user", githubUser);
             return "redirect:/";
         } else {
             // 登录失败，重新登录
